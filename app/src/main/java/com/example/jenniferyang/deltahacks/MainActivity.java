20f14da371a965188f64d77379276489927c452e
jenniferyang <jennyyang01@gmail.com>
2019-01-26 22:19:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jenniferyang/deltahacks/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jenniferyang.deltahacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference usersRef = database.getReference("User");
+}
